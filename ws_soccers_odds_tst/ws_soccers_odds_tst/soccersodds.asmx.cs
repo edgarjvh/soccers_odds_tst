75754020550cc9add98bdd8c51baba77ac91ac5b
Edgar Villasmil <edgarjvh@gmail.com>
2017-01-18 10:35:52 -0400

    Creacion de la solucion y archivo asmx

---
+++ ws_soccers_odds_tst/ws_soccers_odds_tst/soccersodds.asmx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace ws_soccers_odds_tst
+{
+    /// <summary>
+    /// Descripción breve de soccersodds
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // Para permitir que se llame a este servicio web desde un script, usando ASP.NET AJAX, quite la marca de comentario de la línea siguiente. 
+    [System.Web.Script.Services.ScriptService]
+    public class soccersodds : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public object ReadData(string xmlUrl)
+        {
+            return null;
+        }
+
+
+
+    }
+}
